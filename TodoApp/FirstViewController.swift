4f7412fe6b9a14c0864d10e452cb44efd5583b01
Efren Alejandro Barroso Llanes <efrenalejandrobll@Efrens-Mac.local>
2015-06-12 14:22:11 -0500

    Initial Commit

---
+++ TodoApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TodoApp
+//
+//  Created by Efren Alejandro Barroso Llanes on 12/06/15.
+//  Copyright (c) 2015 Efren Alejandro Barroso Llanes. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TodoApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TodoApp
+//
+//  Created by Efren Alejandro Barroso Llanes on 12/06/15.
+//  Copyright (c) 2015 Efren Alejandro Barroso Llanes. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
